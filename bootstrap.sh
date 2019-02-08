40eb2854512991907b7c7ffd14983d95f8e13c1d
Jasper Ginn <jasperginn@gmail.com>
2019-02-08 15:42:30 +0100

    Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin.bash
+
+# Build 'build' container
+
+# Call build container
+
+# Copy local settings to the 'chitchat' volume using the 'helper' container
+
+# Build 'database' container
+
+# Build 'app' container
+
+# Start the database container
+
+# Configure the database
+
+# Finish 
