abd4dbe55ac312763951dc4b00c8a4fc0a20af6f
Jasper Ginn <jasperginn@gmail.com>
2019-02-08 18:58:52 +0100

    Add rebuild script

---
+++ rebuild.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Rebuild the ChitChat jar file
+docker build build/. -t chitchat/chitchat_build
+docker run --rm --mount source=chitchat,target=/var/chitchat chitchat/chitchat_build
+
+# Retrieve the new VERSION.txt file
+docker run --mount source=chitchat,target=/var/chitchat --name helper busybox
+docker cp helper:/var/chitchat/VERSION.txt VERSION.txt
+docker rm helper
+
+# Read VERSION
+ccversion='cat VERSION.txt'
+
+echo "Finished building ${ccversion}"
