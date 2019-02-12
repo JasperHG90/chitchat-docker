a2b9aabb3bf3e3fd35074fda3a219d4654a0d70e
Jasper Ginn <jasperginn@gmail.com>
2019-02-12 14:32:05 +0100

    Rebuild should always try to migrate data

--- rebuild.sh
+++ rebuild.sh
@@ -10,6 +10,15 @@ docker cp helper:/var/chitchat/VERSION.txt VERSION.txt
 docker cp settings.yml helper:/var/chitchat/settings.yml
 docker rm helper
 
+# Start the database container
+docker run -d --rm --mount source=chitchat_postgres,target=/var/lib/postgresql/data --env-file env.list --name chitchat_database chitchat/chitchat_db
+
+# Configure the database
+docker run --rm --mount source=chitchat,target=/var/chitchat --link=chitchat_database:database chitchat/chitchat_app db migrate settings.yml
+
+# Stop the database
+docker stop chitchat_database
+
 # Read VERSION
 ccversion=$( cat VERSION.txt )
 
